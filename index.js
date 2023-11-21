5a558d3faf4b959594360cc96742026431ed3efe
sour-sauce1945 <jayeshdeshmukh509@gmail.com>
2023-11-21 12:20:57 +0530

    added js script

---
+++ index.js
@@ -0,0 +1,9 @@
+function sendMail(){
+    var params = {
+        firstname :document.getElementById("fname").value ,
+        lastname : document.getElementById("lname").value,
+    };
+}
+
+const serviceID = "service_9zysyu4";
+const templateID = ""
