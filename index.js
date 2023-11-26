c30cb96a8fe7cb341cd318574e4e1848217768e0
sour-sauce1945 <jayeshdeshmukh509@gmail.com>
2023-11-26 21:38:12 +0530

    Final Commit , all problems fixed , fully functional

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function sendMail(){
+    
     var params = {
         firstname :document.getElementById("fname").value ,
         lastname : document.getElementById("lname").value,
+        email : document.getElementById("email").value,
+        gender: document.getElementById("gender").value,
+        dateofbirth : document.getElementById("birthday").value,
+        academicyear: document.getElementById("year").value,
+        contact1 : document.getElementById("snumber").value,
+        contact2 : document.getElementById("pnumber").value,
+        residence : document.getElementById("address").value
     };
+    const serviceID = "service_9zysyu4";
+const templateID = "template_fkd2kvn";
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("fname").value="";
+        document.getElementById("lname").value="";
+        console.log(res);
+        alert("message sent successfully")
+    }
+)
+.catch(err=>console.log(err));
 }
 
-const serviceID = "service_9zysyu4";
-const templateID = ""
