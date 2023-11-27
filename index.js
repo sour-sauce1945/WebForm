b070db9a80e7011c23e140cdfc0b04ced5670a07
sour-sauce1945 <jayeshdeshmukh509@gmail.com>
2023-11-27 13:52:48 +0530

    tweaks

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function sendMail(){
     
     var params = {
-        firstname :document.getElementById("fname").value ,
-        lastname : document.getElementById("lname").value,
-        email : document.getElementById("email").value,
-        gender: document.getElementById("gender").value,
-        dateofbirth : document.getElementById("birthday").value,
-        academicyear: document.getElementById("year").value,
-        contact1 : document.getElementById("snumber").value,
-        contact2 : document.getElementById("pnumber").value,
-        residence : document.getElementById("address").value
+        firstname: document.getElementById("fname").value,
+        lastname: document.getElementById("lname").value,
+        email: document.getElementById("email").value,
+        gender: document.querySelector('input[name="gender"]:checked').value,
+        dateofbirth: document.getElementById("birthday").value,
+        academicyear: document.querySelector('input[name="Year"]:checked').value,
+        contact1: document.getElementById("snumber").value,
+        contact2: document.getElementById("pnumber").value,
+        residence: document.getElementById("address").value,
     };
+    
     const serviceID = "service_9zysyu4";
 const templateID = "template_fkd2kvn";
 
